4be9c4244b7ecf4ff166914aba2d38b3a6ea0220
Annie Huang <annie.hn@gmail.com>
2026-01-09 20:55:29 +1100

    Define a buttonVariants from cva in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,24 @@
 import Button from './components/Button.tsx';
+import { cva } from 'class-variance-authority';
+
+const buttonVariants = cva('inline-flex justify-center items-center', {
+  variants: {
+    variant: {
+      default: '',
+      primary: '',
+      destructive: '',
+      warning: '',
+    },
+    size: {
+      default: 'h-12 px-4',
+      icon: 'h-12 w-12',
+    },
+  },
+  defaultVariants: {
+    variant: 'default',
+    size: 'icon',
+  },
+});
 
 function App() {
   return (
