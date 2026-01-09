98c2ba4a59562a6e00cca36a1d5504f84b607029
Annie Huang <annie.hn@gmail.com>
2026-01-09 21:53:23 +1100

    Prove I can still overwrite it with inline styling.

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,12 @@ function App() {
       <h1>Hello there</h1>
       <div className='space-x-2'>
         {/*<Button className='text-primary'>Submit</Button>*/}
-        <Button variant={'primary'} className='bg-amber-900'>
+        {/*<Button variant={'primary'} className='bg-amber-900'>Submit</Button>*/}
+        <Button
+          variant={'primary'}
+          className='bg-amber-900'
+          style={{ backgroundColor: 'green' }}
+        >
           Submit
         </Button>
         <Button variant={'destructive'}>Cancel</Button>
