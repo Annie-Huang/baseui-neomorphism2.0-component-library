526f309fe3e4a8009728e8d80718e7a7899ad744
Annie Huang <annie.hn@gmail.com>
2026-01-10 22:01:52 +1100

    Shows how to make a circle button

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -6,7 +6,7 @@ const Nav = () => {
   return (
     <nav className='py-8 px-12 flex justify-between items-center'>
       <h1 className='font-bold'>Neumorphism</h1>
-      <Button>
+      <Button shape={'circle'} size={'icon'}>
         <Sun className='h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90' />
         <Moon className='absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0' />
       </Button>
