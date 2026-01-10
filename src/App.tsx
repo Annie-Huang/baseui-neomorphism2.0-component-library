30291ea7dfa297abdfa0d52b6daf1678ebdb6c8f
Annie Huang <annie.hn@gmail.com>
2026-01-10 20:45:19 +1100

    Add default and warning button into App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,13 @@ function App() {
     <main>
       <h1>Hello there</h1>
       <div className='space-x-2'>
+        <Button>Submit</Button>
         {/*<Button className='text-primary'>Submit</Button>*/}
         {/*<Button variant={'primary'} className='bg-amber-900'>Submit</Button>*/}
         {/*<Button variant={'primary'} className='bg-amber-900' style={{ backgroundColor: 'green' }}>Submit</Button>*/}
         <Button variant={'primary'}>Submit</Button>
         <Button variant={'destructive'}>Cancel</Button>
+        <Button variant={'warning'}>Warning</Button>
       </div>
     </main>
   );
