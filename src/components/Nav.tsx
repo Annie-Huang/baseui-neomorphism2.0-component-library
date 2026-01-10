09b4ad689e8a52c71dde7aa4e610911f71e6e1aa
Annie Huang <annie.hn@gmail.com>
2026-01-10 22:17:38 +1100

    Display different icon depend on the theme value.

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -20,8 +20,15 @@ const Nav = () => {
         shape={'circle'}
         size={'icon'}
       >
-        <Sun className='h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90' />
-        <Moon className='absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0' />
+        {theme === 'light' ? (
+          // Need to add @custom-variant dark (&:where(.dark, .dark *)); in index.css otherwise it will not recognize what dark: means
+          <Sun className='h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90 text-warning' />
+        ) : (
+          <Moon
+            className='absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0'
+            text-primary
+          />
+        )}
       </Button>
     </nav>
   );
