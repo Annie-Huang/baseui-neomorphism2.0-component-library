c632f76d2f445bfa6873e11cce0f462bc304eef8
Annie Huang <annie.hn@gmail.com>
2026-01-10 22:09:45 +1100

    Add logic to detect theme and setTheme when teh button is onClicked. Tested and it's working.

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,12 +1,25 @@
 // This is mirror Add a mode toggle in https://ui.shadcn.com/docs/dark-mode/vite
 import Button from './Button.tsx';
 import { Moon, Sun } from 'lucide-react';
+import { useTheme } from '../providers/theme-provider.tsx';
 
 const Nav = () => {
+  const { setTheme, theme } = useTheme();
+
   return (
     <nav className='py-8 px-12 flex justify-between items-center'>
       <h1 className='font-bold'>Neumorphism</h1>
-      <Button shape={'circle'} size={'icon'}>
+      <Button
+        onClick={() => {
+          if (theme === 'light') {
+            setTheme('dark');
+          } else {
+            setTheme('light');
+          }
+        }}
+        shape={'circle'}
+        size={'icon'}
+      >
         <Sun className='h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90' />
         <Moon className='absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0' />
       </Button>
