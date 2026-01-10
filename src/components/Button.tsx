fcd6dccb865e3efa437b2244ac1f4622bdcb1722
Annie Huang <annie.hn@gmail.com>
2026-01-10 21:19:26 +1100

    Make it smaller when it is pressed and enhance the animation

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -32,7 +32,7 @@ const buttonVariants = cva(
   'inline-flex justify-center items-center cursor-pointer hover:opacity-90 focus-visible:ring-foreground ' +
     'focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none border-3 border-foreground ' +
     'border-background shadow-raised bg-linear-to-tl disabled:opacity-50 disabled:pointer-events-none font-medium ' +
-    'active:shadow-inset transition-all duration-250 ease-out',
+    'active:shadow-inset transition-all duration-250 ease-out active:scale-97',
   {
     variants: {
       variant: {
