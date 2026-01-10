efdbcdc666a8fe7519076c8dabe2e8a790833ead
Annie Huang <annie.hn@gmail.com>
2026-01-10 22:00:15 +1100

    Do some basic styling for the <Nav>

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,8 +4,8 @@ import { Moon, Sun } from 'lucide-react';
 
 const Nav = () => {
   return (
-    <nav className='py-4 px-12'>
-      <h1>Neumorphism</h1>
+    <nav className='py-8 px-12 flex justify-between items-center'>
+      <h1 className='font-bold'>Neumorphism</h1>
       <Button>
         <Sun className='h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90' />
         <Moon className='absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0' />
