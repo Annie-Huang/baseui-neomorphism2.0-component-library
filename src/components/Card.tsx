c0171fe7ddf31a92ef69ea8c7f40f9ded4669523
Annie Huang <annie.hn@gmail.com>
2026-01-11 12:33:25 +1100

    We don't need variants for carHeaderVariants, remove it.

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -27,12 +27,7 @@ export const Card = (props: CardProps) => {
 };
 
 // Card Header
-const cardHeaderVariants = cva('rounded-2xl border-3 border-border', {
-  variants: { variant: { raised: 'shadow-raised', inset: 'shadow-inset' } },
-  defaultVariants: {
-    variant: 'raised',
-  },
-});
+const cardHeaderVariants = cva('flex flex-col, space-y-1.5 p-6');
 
 interface CardHeaderProps
   extends useRender.ComponentProps<'div'>,
@@ -40,7 +35,7 @@ interface CardHeaderProps
 
 export const CardHeader = (props: CardHeaderProps) => {
   const mergedProps = mergeProps(props, {
-    className: cardHeaderVariants({ variant: props.variant }),
+    className: cardHeaderVariants(),
   });
 
   const element = useRender({
