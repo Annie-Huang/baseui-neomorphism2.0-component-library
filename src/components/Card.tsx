da6b1117e2d886948b823f0c066d0ae39121feb9
Annie Huang <annie.hn@gmail.com>
2026-01-11 12:26:42 +1100

    Copy the whole set and paste it at the end of the file for Card.tsx

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -25,3 +25,29 @@ export const Card = (props: CardProps) => {
 
   return element;
 };
+
+// Card Header
+const cardVariants = cva('rounded-2xl border-3 border-border', {
+  variants: { variant: { raised: 'shadow-raised', inset: 'shadow-inset' } },
+  defaultVariants: {
+    variant: 'raised',
+  },
+});
+
+interface CardProps
+  extends useRender.ComponentProps<'div'>,
+    VariantProps<typeof cardVariants> {}
+
+export const Card = (props: CardProps) => {
+  const mergedProps = mergeProps(props, {
+    className: cardVariants({ variant: props.variant }),
+  });
+
+  const element = useRender({
+    defaultTagName: 'div',
+    render: props.render,
+    props: mergedProps,
+  });
+
+  return element;
+};
