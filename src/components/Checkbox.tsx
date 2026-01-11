5c830627be9506e6e66a84023dfc68ec20af0348
Annie Huang <annie.hn@gmail.com>
2026-01-11 20:54:47 +1100

    Create Checkbox.tsx which will use <Checkbox> from base-ui

---
+++ src/components/Checkbox.tsx
@@ -0,0 +1,11 @@
+import { Checkbox } from '@base-ui/react';
+
+const NeuCheckbox = () => {
+  return (
+    <Checkbox.Root>
+      <Checkbox.Indicator />
+    </Checkbox.Root>
+  );
+};
+
+export default NeuCheckbox;
