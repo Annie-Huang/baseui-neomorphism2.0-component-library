f1ef4e8f72c6436611f3412407a125fa96b98e95
Annie Huang <annie.hn@gmail.com>
2026-01-11 15:25:32 +1100

    Type the props for NeuSwitch

--- src/components/Switch.tsx
+++ src/components/Switch.tsx
@@ -1,8 +1,17 @@
 import { Switch, useRender } from '@base-ui/react';
+import { cva, type VariantProps } from 'class-variance-authority';
 
-const NeuSwitch = () => {
+const switchVariants = cva('cursor-pointer');
+
+// I got confused, we don't have a html element called 'switch', there is a material ui Switch component as well as React Native Switch component.
+// But I actually don't know what is the React.ElementType or HTMLProps of 'switch' is.
+interface SwitchProps
+  extends useRender.ComponentProps<'switch'>,
+    VariantProps<typeof switchVariants> {}
+
+const NeuSwitch = (props: SwitchProps) => {
   return (
-    <Switch.Root>
+    <Switch.Root {...props}>
       <Switch.Thumb />
     </Switch.Root>
   );
