5a1c35c3c658eceaf4cd1cfa05d4b3d87e4cac65
Annie Huang <annie.hn@gmail.com>
2026-01-11 21:06:59 +1100

    Create checkboxVariants

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import { Checkbox } from '@base-ui/react';
+import { cva } from 'class-variance-authority';
+
+const checkboxVariants = cva(
+  'h-5 w-5 shrink-0 rounded-sm border-1 border-highlight bg-background shadow-inset focus-visible:outline-none ' +
+    'focus-visible:ring-2 focus-visible:ring-foreground focus-visible:ring-offset-2',
+);
 
 // Since it does not have 'checkbox' so we will use typeof Checkbox.Root
 interface CheckBoxProps extends React.ComponentProps<typeof Checkbox.Root> {
