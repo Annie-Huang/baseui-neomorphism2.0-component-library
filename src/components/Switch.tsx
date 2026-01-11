d5eaec9d7378c9ccd4f868b5d32e1653e746c453
Annie Huang <annie.hn@gmail.com>
2026-01-11 15:16:49 +1100

    Create Switch.tsx to use <Switch> in base-ui

---
+++ src/components/Switch.tsx
@@ -0,0 +1,11 @@
+import { Switch, useRender } from '@base-ui/react';
+
+const NeuSwitch = () => {
+  return (
+    <Switch.Root>
+      <Switch.Thumb />
+    </Switch.Root>
+  );
+};
+
+export default NeuSwitch;
