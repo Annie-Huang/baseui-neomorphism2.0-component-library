30cbff00e2976c1eae621ca5cc87a87c9b2f9c65
Annie Huang <annie.hn@gmail.com>
2026-01-11 21:01:51 +1100

    Create CheckBoxProps and use it to type NeuCheckbox

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,6 +1,12 @@
+import React from 'react';
 import { Checkbox } from '@base-ui/react';
 
-const NeuCheckbox = () => {
+// Since it does not have 'checkbox' so we will use typeof Checkbox.Root
+interface CheckBoxProps extends React.ComponentProps<typeof Checkbox.Root> {
+  className?: string;
+}
+
+const NeuCheckbox = (props: CheckBoxProps) => {
   return (
     <Checkbox.Root>
       <Checkbox.Indicator />
