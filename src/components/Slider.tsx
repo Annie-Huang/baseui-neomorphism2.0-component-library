ecd9f2d3fd739efdf7cc43f90c80b310e67d3b33
Annie Huang <annie.hn@gmail.com>
2026-01-12 20:15:02 +1100

    Add Slider.tsx file that will use the Slider from base-ui

---
+++ src/components/Slider.tsx
@@ -0,0 +1,15 @@
+import { Slider } from '@base-ui/react';
+
+const NeuSlider = (props) => {
+  return (
+    <Slider.Root>
+      <Slider.Control>
+        <Slider.Track>
+          <Slider.Indicator />
+        </Slider.Track>
+      </Slider.Control>
+    </Slider.Root>
+  );
+};
+
+export default NeuSlider;
