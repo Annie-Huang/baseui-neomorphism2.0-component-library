297c537744adac5d6b1fa4edde4fc4c5e6740fdf
Annie Huang <annie.hn@gmail.com>
2026-01-12 20:25:13 +1100

    Create the SliderVariants and SliderProps and use it to type the props pass into NeuSlider

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,6 +1,16 @@
 import { Slider } from '@base-ui/react';
+import React from 'react';
+import { cva, type VariantProps } from 'class-variance-authority';
 
-const NeuSlider = (props) => {
+const sliderVariants = cva('w-full h-4');
+
+const trackVariants = cva('relative w-full rounded-full shadow-inset h-4');
+
+interface SliderProps
+  extends React.ComponentProps<typeof Slider.Root>,
+    VariantProps<typeof sliderVariants> {}
+
+const NeuSlider = (props: SliderProps) => {
   return (
     <Slider.Root>
       <Slider.Control>
