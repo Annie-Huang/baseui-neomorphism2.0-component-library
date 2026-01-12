4b8f99d795e3d9ad08be27d32262919e70c49933
Annie Huang <annie.hn@gmail.com>
2026-01-12 20:28:04 +1100

    Add rangeVariants into Slider.tsx

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -6,6 +6,18 @@ const sliderVariants = cva('w-full h-4');
 
 const trackVariants = cva('relative w-full rounded-full shadow-inset h-4');
 
+const rangeVariants = cva('absolute h-full rounded-full', {
+  variants: {
+    variant: {
+      default: 'bg-linear-to-l from-primary to-primary/75',
+      secondary: 'bg-linear-to-l from-muted to-muted/75',
+    },
+  },
+  defaultVariants: {
+    variant: 'default',
+  },
+});
+
 interface SliderProps
   extends React.ComponentProps<typeof Slider.Root>,
     VariantProps<typeof sliderVariants> {}
