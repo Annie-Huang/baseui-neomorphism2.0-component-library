b378753e199aefa5b6a2c1e94700b6131536fcef
Annie Huang <annie.hn@gmail.com>
2026-01-12 21:41:47 +1100

    Create Progress.tsx component that use Progress from base-ui

---
+++ src/components/Progress.tsx
@@ -0,0 +1,13 @@
+import { Progress } from '@base-ui/react';
+
+const NeuProgress = () => {
+  return (
+    <Progress.Root>
+      <Progress.Track>
+        <Progress.Indicator />
+      </Progress.Track>
+    </Progress.Root>
+  );
+};
+
+export default NeuProgress;
