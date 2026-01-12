6535d83ad19279ea57362a4c311738bfbd64a949
Annie Huang <annie.hn@gmail.com>
2026-01-12 21:51:07 +1100

    Adding stylings for progressVariants

--- src/components/Progress.tsx
+++ src/components/Progress.tsx
@@ -1,7 +1,10 @@
 import { Progress, useRender } from '@base-ui/react';
 import { cva, type VariantProps } from 'class-variance-authority';
 
-const progressVariants = cva('');
+const progressVariants = cva(
+  'relative overflow-hidden rounded-full bg-background shadow-inset p-0.25 focus:outline-none focus-visible:ring-2 ' +
+    'focus-visible:ring-primary focus-visible:ring-offset-2',
+);
 
 // Great that useRender got 'progress' component
 interface ProgressProps
