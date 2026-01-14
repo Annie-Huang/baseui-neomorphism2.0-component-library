1f66524fc8d2ebe80c86f095021bfd6650025c86
Annie Huang <annie.hn@gmail.com>
2026-01-14 21:02:40 +1100

    Create TabRootProps and type it with <TabsRoot> props

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -1,5 +1,11 @@
 import { Tabs } from '@base-ui/react';
+import React from 'react';
 
-export const TabsRoot = () => {
-  return <Tabs.Root />;
+// interface TabRootProps extends React.ComponentProps<typeof Tabs.Root> {}
+// Change to the following to get rid of ESLint error of:
+// ESLint: An interface declaring no members is equivalent to its supertype. (@typescript-eslint/no-empty-object-type)
+type TabRootProps = React.ComponentProps<typeof Tabs.Root>;
+
+export const TabsRoot = (props: TabRootProps) => {
+  return <Tabs.Root {...props} />;
 };
