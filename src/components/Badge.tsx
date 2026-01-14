27e415a829bf3d69db5609f2f0891689b7da9cce
Annie Huang <annie.hn@gmail.com>
2026-01-14 19:59:44 +1100

    Create variants and defaultVariants for badgeVariants

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -3,6 +3,22 @@ import { cva } from 'class-variance-authority';
 const badgeVariants = cva(
   'inline-flex items-center rounded-full border-2 border-background px-2.5 py-0.5 text-sm font-semibold ' +
     'transition-all duration-200 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2',
+  {
+    variants: {
+      variant: {
+        default: 'bg-muted text-foreground shadow-raised hover:shadow-inset',
+        primary:
+          'bg-linear-to-tl from-primary/5 to-primary/25 text-primary shadow-raised hover:shadow-inset',
+        destructive:
+          'bg-linear-to-tl from-destructive/5 to-destructive/25 text-destructive shadow-raised hover:shadow-inset',
+        warning:
+          'bg-linear-to-tl from-warning/5 to-warning/25 text-warning shadow-raised hover:shadow-inset',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+    },
+  },
 );
 const NeuBadge = () => {
   return <div></div>;
