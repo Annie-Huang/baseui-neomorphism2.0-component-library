c71b28ec0ca5c74b365403435c781979d3c8dd37
Annie Huang <annie.hn@gmail.com>
2026-01-15 20:56:41 +1100

    Make a copy down

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -14,3 +14,16 @@ export const TabsRoot = (props: TabRootProps) => {
 
   return <Tabs.Root {...rootMerge} />;
 };
+
+const tabsRootVariants = cva('w-full');
+
+// interface TabRootProps extends React.ComponentProps<typeof Tabs.Root> {}
+// Change to the following to get rid of ESLint error of:
+// ESLint: An interface declaring no members is equivalent to its supertype. (@typescript-eslint/no-empty-object-type)
+type TabRootProps = React.ComponentProps<typeof Tabs.Root>;
+
+export const TabsRoot = (props: TabRootProps) => {
+  const rootMerge = mergeProps(props, { className: tabsRootVariants() });
+
+  return <Tabs.Root {...rootMerge} />;
+};
